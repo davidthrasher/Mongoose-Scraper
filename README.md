c2ed194698381f10c269fbc86324cc877ee78388
David Thrasher <thrasher1332@hotmail.com>
2017-09-15 23:53:24 -0400

    recommiting to fix heroku upload

---
+++ README.md
@@ -0,0 +1,2 @@
+Metal Scraper is a node application that scrapes articles from Metalsucks.net and stores them into a
+MongoDB database, then serves them to the front end using handlebars.
---
+++ server.js
@@ -0,0 +1,127 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var PORT = process.env.PORT || 3000;
+
+mongoose.Promise = Promise;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static("public"));
+
+// 'mongodb://heroku_kctvqn0f:q3u2tstaa86but2nn5fnd451fv@ds139844.mlab.com:39844/heroku_kctvqn0f'
+mongoose.connect('mongodb://localhost/metalsucksdb');
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+app.get("/scrape", function(req, res) {
+
+  request("http://www.metalsucks.net/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    $("span.post-title").each(function(i, element) {
+      var result = {};
+      result.title = $(element).children().text();
+      result.link = $(element).children().attr("href");
+
+      console.log(result);
+
+      var entry = new Article(result);
+      entry.save(function(err, doc) {
+        {unique: true}
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  res.redirect("/");
+});
+
+app.get("/articles", function(req, res) {
+  Article.find({}, function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+app.get("/articles/:id", function(req, res) {
+  Article.findOne({ "_id": req.params.id })
+  .populate("note")
+  .exec(function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+app.post("/articles/:id", function(req, res) {
+  var newNote = new Note(req.body);
+
+  newNote.save(function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      .exec(function(err, doc) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+app.delete("/delete/:id", function (req, res) {
+  var id = req.params.id.toString();
+  Note.remove({
+    "_id": id
+  }).exec(function (error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log("note deleted");
+      res.redirect("/" );
+    }
+  });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port 3000!");
+});
